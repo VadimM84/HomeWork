9623c3865ea0098faf59485bb8c36ab0a80dfde1
Vadim <vmutovalov24@gmail.com>
2022-12-23 18:43:33 +0500

    add Решение десятой задачи

---
+++ example010/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Clear();
+
+Console.WriteLine("Введите трехзначное число");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if (number < 1000 && number > 99)
+{
+    int number2 = ((number % 100) / 10);
+    Console.WriteLine("Вторая цифра вашего числа");
+    Console.WriteLine(number2);
+}
+else
+{
+    Console.WriteLine("Вы ввеели не трехзначное значное число ало");
+}
